521dd2cccf5ec6dfc087237f5ecee680a7273af0
Denis <denis.shagim@icloud.com>
2024-12-27 13:00:58 +0300

    feat: coplete task

--- src/stack.js
+++ src/stack.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement the Stack with a given interface via array.
@@ -13,22 +13,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.items = []; // Массив для хранения элементов стека
+  }
+
+  push(element) {
+    this.items.push(element); // Добавляем элемент в стек
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.items.pop(); // Удаляем и возвращаем верхний элемент стека
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.items.length === 0) {
+      return undefined; // Если стек пуст, возвращаем undefined
+    }
+    return this.items[this.items.length - 1]; // Возвращаем верхний элемент
   }
 }
 
 module.exports = {
-  Stack
+  Stack,
 };
